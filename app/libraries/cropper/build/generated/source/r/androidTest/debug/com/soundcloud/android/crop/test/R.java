5aa300253db30e14bdf42348f1af62861fa9f248
chenineazeddine <chenineazeddine5@gmail.com>
2017-03-29 14:14:17 +0100

    implementing the help layout

--- app/libraries/cropper/build/generated/source/r/androidTest/debug/com/soundcloud/android/crop/test/R.java
+++ app/libraries/cropper/build/generated/source/r/androidTest/debug/com/soundcloud/android/crop/test/R.java
@@ -80,8 +80,8 @@ containing a value of this type.
         public static final int btn_cancel=0x7f080005;
         public static final int btn_done=0x7f080006;
         public static final int changing=0x7f080001;
-        public static final int crop_image=0x7f080004;
-        public static final int done_cancel_bar=0x7f080003;
+        public static final int crop_image=0x7f080003;
+        public static final int done_cancel_bar=0x7f080004;
         public static final int never=0x7f080002;
     }
     public static final class layout {
--- app/src/main/java/dz/esi/team/appprototype/HelpActivity.java
+++ app/src/main/java/dz/esi/team/appprototype/HelpActivity.java
@@ -1,13 +1,21 @@
 package dz.esi.team.appprototype;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
 
-public class HelpActivity extends AppCompatActivity {
+import com.github.aakira.expandablelayout.ExpandableLayoutListener;
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+import com.github.aakira.expandablelayout.Utils;
+
+public class HelpActivity extends AppCompatActivity{
+
+    ExpandableRelativeLayout expandableLayout1 ,expandableLayout2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +23,21 @@ public class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
+
+    public void expandableButton1(View view) {
+        expandableLayout1 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout1);
+        expandableLayout1.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton2(View view) {
+        expandableLayout2 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout2);
+        expandableLayout2.toggle(); // toggle expand and collapse
+    }
+
+
 
 }
