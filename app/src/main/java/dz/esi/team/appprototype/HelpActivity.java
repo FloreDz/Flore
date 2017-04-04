fd1fdbbf8e1060119bfc7dde08cf4765d002f541
chenineazeddine <chenineazeddine5@gmail.com>
2017-04-04 11:46:28 +0100

    fixing the icone padding bugs

--- app/src/main/java/dz/esi/team/appprototype/HelpActivity.java
+++ app/src/main/java/dz/esi/team/appprototype/HelpActivity.java
@@ -38,16 +38,16 @@ public class HelpActivity extends AppCompatActivity{
         SpannableStringBuilder builder = new SpannableStringBuilder();
 
         builder.append("Que ce soit vous avez son image dans votre galerie");
-        builder.append(",ou vous rencontrez la plante pour la première fois, en cliquant sur le Button ").append("\n  ");
+        builder.append(",ou vous rencontrez la plante pour la première fois, en cliquant sur le Button ").append(" .");
         builder.setSpan(new ImageSpan(HelpActivity.this,R.drawable.ic_fab_icone),
-                builder.length() - 1, builder.length(), 0);
+                builder.length() - 2, builder.length()-1, 0);
         builder.append(" Vous pouvez importer la photo dont vous voulez reconnaitre la plante, ou prendre une photo de cette dernière. Après, l’application va s’occuper de trouver son profil pour vous.");
         textView2.setText(builder);
 
         SpannableStringBuilder builderFour = new SpannableStringBuilder();
-        builderFour.append("Dans ce cas en appuyant sur le Button ").append("\n  ");
+        builderFour.append("Dans ce cas en appuyant sur le Button ").append(" .");
         builderFour.setSpan(new ImageSpan(HelpActivity.this ,R.drawable.search_icone),
-                builderFour.length() -1, builderFour.length(), 0);
+                builderFour.length() -2, builderFour.length()-1, 0);
         builderFour.append(" grâce à une recherche vivante, vous aurez toutes les résultats possibles,après l’introduction de chaque caractère");
 
         textView4.setText(builderFour);
@@ -55,9 +55,9 @@ public class HelpActivity extends AppCompatActivity{
 
 
         SpannableStringBuilder builderFive = new SpannableStringBuilder();
-        builderFive.append("Parce que la plus importante catégorisation et celle des familles, une option est offerte pour faire cela dans la page d’accueil en appuyant sur").append(" ");
+        builderFive.append("Parce que la plus importante catégorisation et celle des familles, une option est offerte pour faire cela dans la page d’accueil en appuyant sur").append(" .");
         builderFive.setSpan(new ImageSpan(HelpActivity.this,R.drawable.option_menu),
-                builderFive.length() - 1, builderFive.length(), 0);
+                builderFive.length() - 2, builderFive.length() -1 , 0);
         builderFive.append(" puis « Afficher par famille »");
 
         textView5.setText(builderFive);
